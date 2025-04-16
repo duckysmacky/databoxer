d03ab1aa9fac588ce6f5a5419b558f394c34e694
DuckySmacky <kriperoka@gmail.com>
2025-04-16 03:05:11 +0300

    Add '--file' flag for 'key set'
    Now you can set the key from file containing a 32-byte hex string representaion of key

--- src/cli/handlers/key.rs
+++ src/cli/handlers/key.rs
@@ -1,5 +1,7 @@
 //! Contains handlers for the key subcommand
 
+use std::fs::File;
+use std::io::Read;
 use clap::ArgMatches;
 use crate::{exits_on, log, options, output};
 
@@ -34,17 +36,35 @@ pub fn handle_key_get(args: &ArgMatches) {
     }
 }
 
-pub fn handle_key_set(args: &ArgMatches) {
-    let new_key = args.get_one::<String>("KEY").expect("Key is required");
-    
+pub fn handle_key_set<'a>(args: &ArgMatches) {
+    let new_key = {
+        if let Some(key_path) = args.get_one::<String>("FILE") {
+            get_key_from_file(key_path).unwrap_or_else(|err| {
+                log!(ERROR, "Unable to read the key file: {}", err);
+                std::process::exit(1)
+            })
+        } else {
+            args.get_one::<String>("KEY").expect("Key is required").to_string()
+        }
+    };
+
     let mut options = options::KeySetOptions::default();
     options.password = args.get_one::<String>("PASSWORD");
 
-    match crate::set_key(new_key, options) {
+    match crate::set_key(&new_key, options) {
         Ok(_) => log!(SUCCESS, "Successfully set a new encryption key for the current profile"),
         Err(err) => {
             log!(ERROR, "Unable to set an encryption key for the current profile");
             exits_on!(err; all);
         }
     }
+}
+
+fn get_key_from_file<'a>(key_path: &String) -> std::io::Result<String> {
+    let mut file = File::open(key_path)?;
+    let mut buffer = vec![0u8; 64];
+    file.read_exact(&mut buffer)?;
+
+    String::from_utf8(buffer)
+        .map_err(|err| std::io::Error::new(std::io::ErrorKind::InvalidData, err))
 }
